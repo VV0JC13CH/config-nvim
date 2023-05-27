424e61ccb6399f093a3afb770c73177e1c549392
VV0JC13CH <wojciech@bitstudio.dev>
2023-05-27 12:40:10 +0200

    Update puppet plugins

--- lua/plugins/puppet.lua
+++ lua/plugins/puppet.lua
@@ -1,7 +1,6 @@
 return {
-'mrk21/yaml-vim',
-'TheLocehiliosan/vim-eyaml',
-'rodjek/vim-puppet',
-'vim-ruby/vim-ruby',
-
+  'mrk21/yaml-vim',
+  'TheLocehiliosan/vim-eyaml',
+  'rodjek/vim-puppet',
+  'vim-ruby/vim-ruby',
 }
