cd52b8d4088a72d2691c7bed0fe6cbfc4a4f3e2f
VV0JC13CH <wojciech@bitstudio.dev>
2023-09-10 18:39:10 +0200

    rust debugger

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -5,32 +5,57 @@ return {
       vim.o.syntax = 'Enable'
       vim.filetype.indend = true
     end,
-    ft = { "rust", "toml" },
     dependecies = {
-      {
-        "simrat39/rust-tools.nvim",
-        dependecies = {
-          'neovim/nvim-lspconfig',
-          'nvim-lua/plenary.nvim',
-          'mfussenegger/nvim-dap',
-        },
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
+      'mfussenegger/nvim-dap',
+    },
+    {
+      "simrat39/rust-tools.nvim",
+      config = function()
+        local dap = require("dap")
+
+        dap.adapters.codelldb = {
+          type = 'server',
+          port = "${port}",
+          executable = {
+            -- Change this to your path!
+            command = 'codelldb',
+            args = { "--port", "${port}" },
+          }
+        }
+
+        dap.configurations.rust = {
+          {
+            name = "Launch file",
+            type = "codelldb",
+            request = "launch",
+            program = function()
+              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            end,
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+          },
+        }
+
+        require("dapui").setup()
+      end
+    },
+    {
+      "Saecki/crates.nvim",
+      dependecies = {
+        "nvim-lua/plenary.nvim"
       },
-      {
-        "Saecki/crates.nvim",
-        dependecies = {
-          "nvim-lua/plenary.nvim"
-        },
-        ft = { "toml" }
-      },
-      {
-        "williamboman/mason.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if type(opts.ensure_installed) == "table" then
-            vim.list_extend(opts.ensure_installed, { "codelldb" })
-          end
-        end,
-      }
+      ft = { "toml" }
+    },
+    {
+      "williamboman/mason.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if type(opts.ensure_installed) == "table" then
+          vim.list_extend(opts.ensure_installed, { "codelldb" })
+        end
+      end,
     }
   }
 }
