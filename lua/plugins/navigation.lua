d9ce68f24b0ff09f64671d182da370a3e299b5b4
wbobrowski <wbobrowski@egnyte.com>
2025-06-12 09:48:42 +0200

    Quick fix Mason setup

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -4,132 +4,204 @@
 --
 
 return {
-  {
-    'kevinhwang91/rnvimr',
-    config = function()
-      vim.keymap.set('n', '<leader>tr', '<cmd>RnvimrToggle<CR>', { desc = 'Toggle Ranger' })
-      -- Make Ranger replace Netrw and be the file explorer
-      vim.g.rnvimr_enable_ex = 1
-    end,
-  },
-  {
-    'ThePrimeagen/harpoon',
-    config = function()
-      vim.keymap.set('n', '<leader>hh', require('harpoon.ui').toggle_quick_menu, { desc = 'Harpoon' })
-      vim.keymap.set('n', '<leader>th', require('harpoon.ui').toggle_quick_menu, { desc = 'Toggle Harpoon' })
-      vim.keymap.set('n', '<C-A-Up>', require('harpoon.ui').nav_prev, { desc = 'Previous harpoon' })
-      vim.keymap.set('n', '<C-A-Down>', require('harpoon.ui').nav_next, { desc = 'Next harpoon' })
-      vim.keymap.set('n', '<leader>h1', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', { desc = '1st harpoon' })
-      vim.keymap.set('n', '<leader>h2', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', { desc = '2nd harpoon' })
-      vim.keymap.set('n', '<leader>h3', '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', { desc = '3rd harpoon' })
-      vim.keymap.set('n', '<leader>h4', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', { desc = '4th harpoon' })
+	{
+		"kevinhwang91/rnvimr",
+		config = function()
+			vim.keymap.set("n", "<leader>tr", "<cmd>RnvimrToggle<CR>", { desc = "Toggle Ranger" })
+			-- Make Ranger replace Netrw and be the file explorer
+			vim.g.rnvimr_enable_ex = 1
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		config = function()
+			vim.keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu, { desc = "Harpoon" })
+			vim.keymap.set("n", "<leader>th", require("harpoon.ui").toggle_quick_menu, { desc = "Toggle Harpoon" })
+			vim.keymap.set("n", "<C-A-Up>", require("harpoon.ui").nav_prev, { desc = "Previous harpoon" })
+			vim.keymap.set("n", "<C-A-Down>", require("harpoon.ui").nav_next, { desc = "Next harpoon" })
+			vim.keymap.set(
+				"n",
+				"<leader>h1",
+				'<cmd>lua require("harpoon.ui").nav_file(1)<CR>',
+				{ desc = "1st harpoon" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>h2",
+				'<cmd>lua require("harpoon.ui").nav_file(2)<CR>',
+				{ desc = "2nd harpoon" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>h3",
+				'<cmd>lua require("harpoon.ui").nav_file(3)<CR>',
+				{ desc = "3rd harpoon" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>h4",
+				'<cmd>lua require("harpoon.ui").nav_file(4)<CR>',
+				{ desc = "4th harpoon" }
+			)
 
-      vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file, { desc = 'Add file to harpoon' })
-      vim.keymap.set('n', '<leader>hr', require('harpoon.mark').rm_file, { desc = 'Remove file from harpoon' })
-    end,
-  },
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-    ,
-    config = function()
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
-      require('telescope').setup {
-        defaults = {
-          mappings = {
-            i = {
-              ['<C-u>'] = false,
-              ['<C-d>'] = false,
-            },
-          },
-        },
-      }
-    end,
-  },
+			vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file, { desc = "Add file to harpoon" })
+			vim.keymap.set("n", "<leader>hr", require("harpoon.mark").rm_file, { desc = "Remove file from harpoon" })
+		end,
+	},
+	-- Fuzzy Finder (files, lsp, etc)
+	-- NOTE: Plugins can specify dependencies.
+	--
+	-- The dependencies are proper plugin specifications as well - anything
+	-- you do for a plugin at the top level, you can do for a dependency.
+	--
+	-- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
-    end,
-    config = function()
-      -- Enable telescope fzf native, if installed
-      pcall(require('telescope').load_extension, 'fzf')
-      vim.keymap.set('n', '<A-C-F>', '<cmd>Files<CR>', { desc = 'Files' })
-      vim.keymap.set('n', '<A-C-G>', '<cmd>GFiles?<CR>', { desc = 'Git files' })
-      vim.keymap.set('n', '<A-C-B>', '<cmd>Buffers<CR>', { desc = 'Buffers' })
+	{ -- Fuzzy Finder (files, lsp, etc)
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+				"nvim-telescope/telescope-fzf-native.nvim",
 
-      pcall(require("telescope").load_extension, 'harpoon')
-      -- See `:help telescope.builtin`
-      vim.keymap.set('n', '<leader>s/', function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = 'Search in current buffer' })
+				-- `build` is used to run some command when the plugin is installed/updated.
+				-- This is only run then, not every time Neovim starts up.
+				build = "make",
 
-      vim.keymap.set('n', '<leader>sG', require('telescope.builtin').git_files, { desc = 'Search Git Files' })
-      vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = 'Search Files' })
-      vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = 'Search Help' })
-      vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = 'Search current Word' })
-      vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = 'Search by Grep' })
-      vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = 'Search Diagnostics' })
-      vim.keymap.set('n', '<leader>so', require('telescope.builtin').oldfiles, { desc = 'Search in Old files' })
-    end,
-  },
-  'junegunn/fzf',
-  {
-    'junegunn/fzf.vim',
-    config = function()
-      -- Enable telescope fzf native, if installed
-      pcall(require('telescope').load_extension, 'fzf')
-      vim.keymap.set('n', '<A-C-F>', '<cmd>Files<CR>', { desc = 'Search Files' })
-      vim.keymap.set('n', '<A-C-G>', '<cmd>GFiles?<CR>', { desc = 'Search Git files' })
-      vim.keymap.set('n', '<A-C-B>', '<cmd>Buffers<CR>', { desc = 'Search Buffers' })
-    end,
+				-- `cond` is a condition used to determine whether this plugin should be
+				-- installed and loaded.
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
 
-  },
-  {
-    'mbbill/undotree',
-    config = function()
-      vim.keymap.set('n', '<leader>tu', '<cmd>UndotreeToggle<CR>', { desc = 'Toggle Undo tree' })
-    end,
-  },
-  {
-    'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = {
-      {
-        '<leader>to',
-        function()
-          vim.cmd("vsplit | wincmd l")
-          require("oil").open()
-        end,
-        desc = 'Toggle Oil',
-      }
-      , {
-      '<leader>o',
-      function()
-        require('oil').open()
-      end,
-      desc = 'oil',
-    }
-    },
-    opts = {
-      default_file_explorer = true,
-      view_options = {
-        show_hidden = true,
-      },
-    },
-  }
+			-- Useful for getting pretty icons, but requires a Nerd Font.
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		},
+		config = function()
+			-- Telescope is a fuzzy finder that comes with a lot of different things that
+			-- it can fuzzy find! It's more than just a "file finder", it can search
+			-- many different aspects of Neovim, your workspace, LSP, and more!
+			--
+			-- The easiest way to use Telescope, is to start by doing something like:
+			--  :Telescope help_tags
+			--
+			-- After running this command, a window will open up and you're able to
+			-- type in the prompt window. You'll see a list of `help_tags` options and
+			-- a corresponding preview of the help.
+			--
+			-- Two important keymaps to use while in Telescope are:
+			--  - Insert mode: <c-/>
+			--  - Normal mode: ?
+			--
+			-- This opens a window that shows you all of the keymaps for the current
+			-- Telescope picker. This is really useful to discover what Telescope can
+			-- do as well as how to actually do it!
+
+			-- [[ Configure Telescope ]]
+			-- See `:help telescope` and `:help telescope.setup()`
+			require("telescope").setup({
+				-- You can put your default mappings / updates / etc. in here
+				--  All the info you're looking for is in `:help telescope.setup()`
+				--
+				-- defaults = {
+				--   mappings = {
+				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+				--   },
+				-- },
+				-- pickers = {}
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+
+			-- Enable Telescope extensions if they are installed
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			-- See `:help telescope.builtin`
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+			-- Slightly advanced example of overriding default behavior and theme
+			vim.keymap.set("n", "<leader>/", function()
+				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
+
+			-- It's also possible to pass additional configuration options.
+			--  See `:help telescope.builtin.live_grep()` for information about particular keys
+			vim.keymap.set("n", "<leader>s/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
+
+			-- Shortcut for searching your Neovim configuration files
+			vim.keymap.set("n", "<leader>sn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[S]earch [N]eovim files" })
+		end,
+	},
+	"junegunn/fzf",
+	{
+		"junegunn/fzf.vim",
+		config = function()
+			-- Enable telescope fzf native, if installed
+			pcall(require("telescope").load_extension, "fzf")
+			vim.keymap.set("n", "<A-C-F>", "<cmd>Files<CR>", { desc = "Search Files" })
+			vim.keymap.set("n", "<A-C-G>", "<cmd>GFiles?<CR>", { desc = "Search Git files" })
+			vim.keymap.set("n", "<A-C-B>", "<cmd>Buffers<CR>", { desc = "Search Buffers" })
+		end,
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>tu", "<cmd>UndotreeToggle<CR>", { desc = "Toggle Undo tree" })
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{
+				"<leader>to",
+				function()
+					vim.cmd("vsplit | wincmd l")
+					require("oil").open()
+				end,
+				desc = "Toggle Oil",
+			},
+			{
+				"<leader>o",
+				function()
+					require("oil").open()
+				end,
+				desc = "oil",
+			},
+		},
+		opts = {
+			default_file_explorer = true,
+			view_options = {
+				show_hidden = true,
+			},
+		},
+	},
 }
